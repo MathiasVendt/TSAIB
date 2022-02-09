d939600c80e84d17a73d93de641b124d2114e835
Mathias Høxbro Juel Vendt <vendyt@hotmail.com>
2022-02-09 14:56:10 +0100

    First commit

---
+++ R/hello.R
@@ -0,0 +1,18 @@
+#' @title Time Series Analysis and Intermission Bias
+#'
+#' @description Estimates a model based on time series analysis, and accounts for
+#' intermission bias of satellite observations.
+#' Input file type: NetCDF (Network Common Data Form).
+#'
+#' @param x The names for the hello function
+#'
+#' @return The output from \code{\link{print}}
+#' @export
+#'
+#' @examples
+#' hello("Stine and Karina")
+#' \dontrun{
+#' hello("Mathias")}
+hello <- function(x) {
+  print(paste0("Hello ", x, ", this is my TSAIB R-package!"))
+}
