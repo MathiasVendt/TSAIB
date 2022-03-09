a3a36f554ea9be615f05ae405d59ac063e0ddef8
Mathias Høxbro Juel Vendt <vendyt@hotmail.com>
2022-03-09 13:13:11 +0100

    change coord example

--- R/GridTSExtract.R
+++ R/GridTSExtract.R
@@ -7,16 +7,15 @@
 #' @param latid The variable id for latitude vector. e.g.: "latitude"
 #' @param timeid The variable id for the time vector. e.g.: "date"
 #' @param measurementsid The variable id for the measurements vector. e.g.: "sea_level_anomaly"
-#' @param coord The center coordinate for the TS grid area. e.g.: c(lon,lat) i.e. c(-165,75)
+#' @param coord The center coordinate for the TS grid area. e.g.: c(lon,lat) i.e. c(-164,74)
 #' @param radius The square radius of the TS grid. e.g: 0=1x1 grid, 1=3x3 grid, 2=5x5 grid etc.
 #' @param dlon Number of data points for each degree longitude
 #' @param dlat Number of data points for each degree latitude
 #'
 #' @return Time series from grid
 #' @export
-GridTSExtract <- function(nco,lonid,latid,timeid,measurementsid,coord,radius,dlon,dlat) {
+GridTSExtract <- function(nco,lonid="longitude",latid="latitude",timeid="date",measurementsid="sea_level_anomaly",coord=c(-165,74),radius=0,dlon=2,dlat=4) {
 library(ncdf4) # package for NetCDF manipulation
-hej=1
 lon =  ncvar_get(nco, varid = lonid) # extracting longitude
 lat =  ncvar_get(nco, varid = latid) # extracting latitude
 date = ncvar_get(nco, varid = timeid)# extracting date
