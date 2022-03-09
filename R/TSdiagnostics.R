7935f97b405848b4328cfc02e491b6c6f1fa2501
Mathias Høxbro Juel Vendt <vendyt@hotmail.com>
2022-03-09 15:35:43 +0100

    Changed parameters to TS only. Inserted basic statistics

--- R/TSdiagnostics.R
+++ R/TSdiagnostics.R
@@ -2,20 +2,23 @@
 #'
 #' @description Shows basic statistics and characteristics of the time series data
 #'
-#' @param nco The NetCDF object (open with "nc_open" from the "ncdf4" library)
-#' @param lonid The variable id for longitude vector. e.g.: "longitude"
-#' @param latid The variable id for latitude vector. e.g.: "latitude"
-#' @param timeid The variable id for the time vector. e.g.: "date"
-#' @param measurementsid The variable id for the measurements vector. e.g.: "sea_level_anomaly"
-#' @param coord The center coordinate for the TS grid area. e.g.: c(lon,lat) i.e. c(-164,74)
-#' @param radius The square radius of the TS grid. e.g: 0=1x1 grid, 1=3x3 grid, 2=5x5 grid etc.
-#' @param dlon Number of data points for each degree longitude
-#' @param dlat Number of data points for each degree latitude
+#' @param TS The time series to be analyzed
 #'
 #' @return Basic statistics and characteristics
 #' @export
 
-TSdiagnostics <- function() {
+TSdiagnostics <- function(TS) {
+# Basic statistics
+cat("mean\n")
+print(mean(TS))
+cat("standard deviation\n")
+print(sd(TS))
+cat("median\n")
+print(median(TS))
+cat("quantile\n")
+print(quantile(TS))
+cat("sum\n")
+print(sum(TS))
 
 
 
