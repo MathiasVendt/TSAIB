a075ee1e2e576589c850f8b8224a85d4785e5991
Mathias Høxbro Juel Vendt <vendyt@hotmail.com>
2022-03-09 16:38:42 +0100

    Added na.rm functionality to remove NaN objects

--- R/TSdiagnostics.R
+++ R/TSdiagnostics.R
@@ -3,23 +3,39 @@
 #' @description Shows basic statistics and characteristics of the time series data
 #'
 #' @param TS The time series to be analyzed
+#' @param nanrem Set NaN to be removed or not. e.g.: nanrem="TRUE"
 #'
 #' @return Basic statistics and characteristics
 #' @export
 
-TSdiagnostics <- function(TS) {
+TSdiagnostics <- function(TS,nanrem="FALSE") {
+
+
+  # Number of NaN's in data set
+  cat("Number of NaN's in the data set\n")
+  print(sum(is.na(TS)))
+
+  # Number of objects in the data set
+  cat("Number of objects in the data set\n")
+  print(length(TS))
+
+  # fraction of data set which is NaN's
+  cat("Fraction of the data set which is NaN's\n")
+  print(sum(is.na(TS))/length(TS))
+
+
 # Basic statistics
 cat("mean\n")
-print(mean(TS))
+print(mean(TS,na.rm=nanrem))
 cat("standard deviation\n")
-print(sd(TS))
+print(sd(TS,na.rm=nanrem))
 cat("median\n")
-print(median(TS))
+print(median(TS,na.rm=nanrem))
 cat("quantile\n")
-print(quantile(TS))
+print(quantile(TS,na.rm=nanrem))
 cat("sum\n")
-print(sum(TS))
-
+print(sum(TS,na.rm=nanrem))
+print(t.test(TS))
 
 
 }
