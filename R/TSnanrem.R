67632cbd5312572d302c3442349e664d18eb30b4
Mathias Høxbro Juel Vendt <vendyt@hotmail.com>
2022-03-13 14:05:30 +0100

    TSnanrem now imputes mean, median and values - and omits on vector time series

--- R/TSnanrem.R
+++ R/TSnanrem.R
@@ -1,20 +1,34 @@
-#' @title Time series NaN remover
+#' @title Time series NaN remover/replacer
 #'
-#' @description Removes NaN from the time series data set, with a specified method of choice
+#' @description Removes or imputes NaN from the time series data set, with a specified method of choice
 #'
-#' @param nco The NetCDF object (open with "nc_open" from the "ncdf4" library)
-#' @param lonid The variable id for longitude vector. e.g.: "longitude"
-#' @param latid The variable id for latitude vector. e.g.: "latitude"
-#' @param timeid The variable id for the time vector. e.g.: "date"
-#' @param measurementsid The variable id for the measurements vector. e.g.: "sea_level_anomaly"
-#' @param coord The center coordinate for the TS grid area. e.g.: c(lon,lat) i.e. c(-164,74)
-#' @param radius The square radius of the TS grid. e.g: 0=1x1 grid, 1=3x3 grid, 2=5x5 grid etc.
-#' @param dlon Number of data points for each degree longitude
-#' @param dlat Number of data points for each degree latitude
+#' @param TS The time series to remove NaN from
+#' @param method The imputation method. e.g.: "omit", "mean", "median" or "value"
+#' @param value_nr The specific value for the "value" imputing method. e.g.: value_nr=0
 #'
-#' @return Time series from grid
+#' @return Time series with NaNs removed or replaced
 #' @export
-TSnanrem <- function() {
+TSnanrem <- function(TS,method="mean",value_nr=0) {
+  if(method=="omit"){
 
-
+    if(is.null(dim(TS))==TRUE){
+      nrnan=sum(is.na(TS))
+      TS=na.omit(TS)
+      print("Number of NaNs removed:")
+      print(nrnan)
+    }
+    else {
+      print("Dimensions are too big, TS must be a vector for omit to work")
+    }
+  }
+  if(method=="mean"){
+  TS[which(is.na(TS)=="TRUE")]=mean(TS,na.rm = "TRUE")
+  }
+  if(method=="median"){
+    TS[which(is.na(TS)=="TRUE")]=median(TS,na.rm = "TRUE")
+  }
+  if(method=="value"){
+    TS[which(is.na(TS)=="TRUE")]=value_nr
+  }
+  return(TS)
 }
