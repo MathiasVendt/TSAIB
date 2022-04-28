f79cb47eea43a0ed82cb8724d29c40339c197682
Mathias Høxbro Juel Vendt <vendyt@hotmail.com>
2022-04-28 15:08:19 +0200

    Added time series data with bias, and bias id

--- R/zzz.R
+++ R/zzz.R
@@ -1,7 +1,7 @@
 
-#' @import TMB
-
-.onLoad <- function(libname,pkgname){
-  cat("Loading compiled code...\n")
-  library.dynam("TSAIB", pkgname, libname)
-}
+##' @import TMB
+#
+#.onLoad <- function(libname,pkgname){
+#  cat("Loading compiled code...\n")
+#  library.dynam("TSAIB", pkgname, libname)
+#}
