5c4a123f966d610279c3735158329bd2e8ceb3db
Mathias Høxbro Juel Vendt <vendyt@hotmail.com>
2022-04-29 09:16:46 +0200

    Added value description in tutorial

--- R/IB.R
+++ R/IB.R
@@ -4,8 +4,8 @@
 #'
 #' @param TSM The time series struct with the intermission bias
 #' @param date The time axis in the time series (dates, seconds, intervals)
-#' @param biasvec The vector containging up to 2 different bias. If the data only contains 1 bias, insert that value instead
-#' @param biasid a struct with the same dimensions as TSM, with integers indicating the bias: 0 for the restrained time series, 1 for the first bias and 2 for the second bias.
+#' @param biasvec The vector containging up to 2 different bias. If the data only contains 1 bias, insert that value instead of the vector
+#' @param biasid A struct with the same dimensions as TSM, with integers indicating the bias: 0 for the restrained time series, 1 for the first bias and 2 for the second bias.
 #' @return Time series struct with bias correction
 #' @export
 
